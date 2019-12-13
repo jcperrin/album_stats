da61d6c5a9de96c558576cfb2d8405bf0a4c66d2
Jean-Christophe Perrin <perrin@kairospower.com>
2019-12-13 14:24:00 -0800

    creating plot of rating frequencies

---
+++ src/analyze_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import seaborn as sns
+
+def import_spreadsheet():
+    """ Reads raw data from some source """
+    return pd.read_csv("../data/2019_bestAlbums.csv")
+
+def clean_spreadsheet(df):
+    """ Takes care of cleaning up the raw data into a more useable form"""
+    return (df.rename(columns = {'JCP Rating' : 'jcp', 
+                         'Chris Rating' : 'chris',
+                         ':) Rating': 'ally'})
+        .drop(df.tail(2).index)
+        )
+
+def plot_histograms(df):
+    """ Creates a histogram of our rating distributions"""
+    view = (df[['jcp', 'chris', 'ally']]
+                #.fillna(0)
+                .melt()
+                )
+    return sns.countplot(x="value", hue="variable", data=view)
+
+
+
+sns.set(context="talk", style="white")
+sns.set_palette("husl", 3)
+df = import_spreadsheet()
+df = clean_spreadsheet(df)
+g = plot_histograms(df)
+sns.despine()
+g.figure.savefig("../results/score_distribution.png", dpi=300, transparent=True)
